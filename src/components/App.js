919763fd3868cd766acf42429602e443c97d025b
darwin chu <darwinchu12@gmail.com>
2021-04-29 06:04:27 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,179 @@
+import $ from 'jquery';
+import 'bootstrap';
+
+import axios from "../http-common";
+import React from 'react';
+import Home from './Home';
+import Users from './Users';
+import Login from './Login';
+import { HashRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+			isAuthenticated: false,
+			token: null,
+			user: {},
+		};
+		this.logout = this.logout.bind(this);
+		this.authenticate = this.authenticate.bind(this);
+		this.parseErrors = this.parseErrors.bind(this);
+    }
+
+    componentWillMount() {
+		const lsToken = localStorage.getItem('jwt'); 
+		const user = localStorage.getItem('user'); 
+
+		if (lsToken) {
+
+			this.authenticate(lsToken, JSON.parse(user));
+		}
+	}
+
+	authenticate(token, user = '') {
+		this.setState({
+			isAuthenticated: true,
+			token: token
+		});
+		localStorage.setItem('jwt', token);
+		axios.defaults.headers.common['Authorization'] = token;
+		if(user) {
+			this.setState({
+				user: user
+			});
+			localStorage.setItem('user', JSON.stringify(user));
+		}
+	}
+
+	logout() {
+		this.setState({
+			isAuthenticated: false,
+			token: null,
+			user: {},
+		});
+		localStorage.removeItem('jwt');
+		localStorage.removeItem('user');
+	}
+
+	refresh() {
+		return axios.get('/api/profile', {
+			headers: { 'Authorization': this.state.token }
+		})
+		.then((response) => {
+			// const token = response.data.token;
+			// this.authenticate(token);
+		})
+		.catch((error) => {
+			console.log('Error!', error);
+		});
+	}
+
+	parseErrors(errors) {
+		let result = '';
+		for(let error in errors){
+		    errors[error].forEach(val => {
+		        result += ('<li>'+val+'</li>');
+		    })
+		}
+
+		return '<ul>' + result + '</ul>';
+	}
+
+    render() {
+        return (
+            <HashRouter>
+			    <div>
+			        <Menu isAuthenticated={this.state.isAuthenticated} user={this.state.user} logout={this.logout} />
+			        <Switch>
+			            <Route exact path='/' component={Home} />
+			            <Route exact path='/login' render={(props) => <Login authenticate={this.authenticate} isAuthenticated={this.state.isAuthenticated} {...props} />} />
+			            <PrivateRoute 
+			            	exact path='/users' 
+			            	component={Users} 
+			            	user={this.state.user} 
+			            	isAuthenticated={this.state.isAuthenticated} 
+			            	token={this.state.token} 
+			            	refresh={this.refresh} 
+			            	parseErrors={this.parseErrors}
+		            	/>
+			        </Switch>
+			    </div>
+			</HashRouter>
+        );
+    }
+}
+
+const PrivateRoute = ({ component: Component, isAuthenticated, token, ...rest }) => (
+	<Route {...rest} render={props => (
+		isAuthenticated ? (
+			<Component {...props} {...rest} token={token} isAuthenticated={isAuthenticated} />
+		) : (
+			<Redirect to={{
+				pathname: '/login',
+				state: { from: props.location }
+			}} />
+		)
+	)} />
+);
+
+const Menu = (props) => (
+	<nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+		<div className="container">
+            <a className="navbar-brand" href="#">
+                Yondu
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            	<ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+	                    <NavLink exact className="nav-link" activeClassName="active" to="/">
+		               		Home
+		           		</NavLink>
+                    </li>
+
+                    {props.isAuthenticated && props.user.role == 'admin' ?
+                    <li className="nav-item">
+	                    <NavLink exact className="nav-link" activeClassName="active" to="/users">
+		               		Users
+		           		</NavLink>
+                    </li>
+                    : null }
+
+                </ul>
+                {<ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"></a>
+                    </li>
+
+                    {props.isAuthenticated ?
+                	<li className="nav-item dropdown">
+                        <a id="navbarDropdown" className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {props.user.first_name} {props.user.last_name} <span className="caret"></span>
+                        </a>
+
+                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="#" onClick={props.logout}>
+                                Logout
+                            </a>
+
+                        </div>
+                    </li>
+                    : 
+                    <li className="nav-item">
+	                    <NavLink exact className="nav-link" activeClassName="active" to="/login">
+		               		Login
+		           		</NavLink>
+                    </li>
+                	}
+                    
+                </ul>}
+			</div>
+		</div>
+	</nav>
+);
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,101 @@
+import axios from "../http-common";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Login extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+			password: '',
+			error: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		const name = event.target.name;
+		this.setState({
+			[name]: event.target.value
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		axios.post('/api/signin', {
+			username: this.state.username,
+			password: this.state.password
+		})
+		.then((response) => {
+			this.setState({ error: '' });
+			const token = response.data.token;
+			const user = response.data.user;
+			this.props.authenticate(token, user);
+		})
+		.catch((error) => {
+
+			this.setState({ error: 'Username or password is not recognised.' });
+
+		});
+	}
+
+	render() {
+		if (this.props.isAuthenticated) {
+			return (
+				<Redirect to={'/'} />
+			);
+		}
+		return (
+			<div className="container py-4">
+				<div className="row justify-content-center">
+			        <div className="col-md-8">
+			            <div className="card">
+							<div className="card-header">Login</div>
+							<div className="card-body">
+								{this.state.error !== '' ?
+									<p className="text-danger">{this.state.error}</p>
+									:
+									null
+								}
+								{this.props.isAuthenticated ?
+									<p className="text-info">You are already logged in.</p>
+									:
+									<form onSubmit={this.handleSubmit}>
+										<div className='form-group'>
+											<input
+												name='username'
+												type='username'
+												className='form-control' 
+												placeholder='Username'
+												value={this.state.username}
+												onChange={this.handleChange} />
+										</div>
+										<div className='form-group'>
+											<input 
+												name='password'
+												type='password' 
+												className='form-control' 
+												placeholder='Password'
+												value={this.state.password}
+												onChange={this.handleChange} />
+										</div>
+
+										<div className="form-group row mb-0">
+                            				<div className="col-md-8">
+												<input type='submit' className='btn btn-primary' value='Login' />
+											</div>
+				                        </div>
+
+									</form>	
+								}
+							</div>
+			            </div>
+		            </div>
+	            </div>
+			</div>
+		);
+	}
+}
+
+export default Login;
